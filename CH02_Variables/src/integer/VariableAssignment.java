fc781c245446eb7035215050d236f3040fd299cd
CBHwang00 <daydreamer00@naver.com>
2021-10-13 12:31:45 +0900

    10월13일오전수업

---
+++ CH02_Variables/src/integer/VariableAssignment.java
@@ -0,0 +1,20 @@
+package integer;
+
+public class VariableAssignment {
+
+	public static void main(String[] args) {
+		// 변수는 항상 재할당(값을 새로 입력가능) 할수있다.
+		int dogs = 8;
+		int cats = 3;
+		
+		System.out.println("1. dogs: " + dogs);
+		System.out.println("2. cats: " + cats);
+
+		dogs = cats;
+		cats = 10;
+		
+		System.out.println("1. dogs: " + dogs);
+		System.out.println("2. cats: " + cats);
+	}
+
+}
